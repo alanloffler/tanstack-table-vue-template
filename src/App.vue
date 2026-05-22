d26072fa854c9a55f764261e5c1ae4c9e34a98c2
alanloffler <alanmatiasloffler@gmail.com>
2026-05-22 18:05:14 -0300

    feat(app): layout

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+</script>
 
-<template></template>
+<template>
+  <div class="bg-background text-foreground min-h-screen">
+    <header class="border-border border-b">
+      <div
+        class="mx-auto flex h-14 max-w-6xl items-center justify-between px-6"
+      >
+        <h1 class="text-lg font-semibold tracking-tight">Tanstack Table</h1>
+      </div>
+    </header>
+    <main class="mx-auto flex max-w-6xl flex-col gap-10 px-6 py-8">
+      <Card class="flex flex-col gap-3">
+        <CardHeader>
+          <CardTitle>Opciones de la tabla</CardTitle>
+        </CardHeader>
+        <CardContent> Content </CardContent>
+      </Card>
+    </main>
+  </div>
+</template>
