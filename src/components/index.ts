cdf46e6db1a1041eee038bfca7d75c7981aa9617
alanloffler <alanmatiasloffler@gmail.com>
2026-05-23 19:54:43 -0300

    feat(barrel): export SearchInput component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as DataTable } from "@/components/DataTable.vue";
 export { default as Pagination } from "@/components/Pagination.vue";
+export { default as SearchInput } from "@/components/SearchInput.vue";
 export { default as SortableIcon } from "@/components/SortableIcon.vue";
