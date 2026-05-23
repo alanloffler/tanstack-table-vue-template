43a2897a5a41fab00c5d79dc6d6fd6e465bc154b
alanloffler <alanmatiasloffler@gmail.com>
2026-05-23 16:55:19 -0300

    feat(barrel): export SortableIcon component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as DataTable } from "@/components/DataTable.vue";
 export { default as Pagination } from "@/components/Pagination.vue";
+export { default as SortableIcon } from "@/components/SortableIcon.vue";
