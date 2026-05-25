70a02418687fa669bb7e136e496fd5c92829dd39
alanloffler <alanmatiasloffler@gmail.com>
2026-05-25 16:20:31 -0300

    feat(barrel): export github icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as FilePdf } from "@/components/icons/FilePdf.vue";
 export { default as FileXls } from "@/components/icons/FileXls.vue";
+export { default as Github } from "@/components/icons/Github.vue";
