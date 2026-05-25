3619f76da902803c8b825990201e842f1a4f0fde
alanloffler <alanmatiasloffler@gmail.com>
2026-05-25 17:02:28 -0300

    feat(components): toggle theme

---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Moon, Sun } from "@lucide/vue";
+
+import { Button } from "@/components/ui/button";
+
+import { useColorMode } from "@vueuse/core";
+
+const mode = useColorMode();
+
+function toggleTheme(e: MouseEvent): void {
+  document.documentElement.style.setProperty("--x", `${e.clientX}px`);
+  document.documentElement.style.setProperty("--y", `${e.clientY}px`);
+  document.startViewTransition(() => (mode.value === "dark" ? (mode.value = "light") : (mode.value = "dark")));
+}
+</script>
+
+<template>
+  <Button @click="(e: MouseEvent) => toggleTheme(e)" size="icon-sm" variant="outline">
+    <Sun v-if="mode === 'dark'" class="stroke-yellow-400" stroke-width="1.5" />
+    <Moon v-else class="fill-neutral-200 stroke-neutral-400" stroke-width="1.5" />
+  </Button>
+</template>
