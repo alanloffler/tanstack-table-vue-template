1fa5f80d09f850eb3e97215690ff00f9b44c2abf
alanloffler <alanmatiasloffler@gmail.com>
2026-05-24 22:28:31 -0300

    feat(components): column sizing

--- src/components/DraggableColumnHeader.vue
+++ src/components/DraggableColumnHeader.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts" generic="TData, TValue">
+import { TableHead } from "@/components/ui/table";
+
 import { FlexRender, type Header } from "@tanstack/vue-table";
 import { ref } from "vue";
 import { useSortable } from "@dnd-kit/vue/sortable";
 
-import { TableHead } from "@/components/ui/table";
+import { cn } from "@/lib/utils";
 
 interface IProps {
+  columnSizing?: boolean;
   header: Header<TData, TValue>;
   index: number;
   isLastColumn?: boolean;
@@ -38,5 +41,17 @@ const { isDragging } = useSortable({
     <span v-if="!header.isPlaceholder" ref="handleEl" class="inline-flex w-fit cursor-grab active:cursor-grabbing">
       <FlexRender :render="header.column.columnDef.header" :props="header.getContext()" />
     </span>
+    <div
+      v-if="!isLastColumn && columnSizing"
+      @dblclick="header.column.resetSize()"
+      @mousedown.stop="header.getResizeHandler()($event)"
+      @touchstart.stop="header.getResizeHandler()($event)"
+      :class="
+        cn(
+          'hover:bg-primary/50 active:bg-primary absolute top-0 right-0 h-full w-0.75 cursor-col-resize touch-none bg-transparent transition-colors select-none',
+          header.column.getIsResizing() && 'bg-primary',
+        )
+      "
+    />
   </TableHead>
 </template>
