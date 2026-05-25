829c11b9d4d62f8b0962da64e65bf26cfeb88f2b
alanloffler <alanmatiasloffler@gmail.com>
2026-05-24 23:11:44 -0300

    feat(types): alignment meta prop

--- src/table.d.ts
+++ src/table.d.ts
@@ -2,6 +2,7 @@ import "@tanstack/vue-table";
 
 declare module "@tanstack/vue-table" {
   interface ColumnMeta {
+    alignment?: "left" | "center" | "right";
     disableDragging?: boolean;
     disableExport?: boolean;
   }
